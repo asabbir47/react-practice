cf3a403d650602a3dc6a8688b9ddd7905cc4d4f2
asabbir47 <asabbir47@gmail.com>
2021-02-22 21:17:06 +0600

    initial

---
+++ discord-widget.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <iframe src="https://discord.com/widget?id=785566305042300979&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+    </body>
+</html>
